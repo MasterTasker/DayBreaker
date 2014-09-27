f644abe5eb56794c4a397e7b873051dc03a54333
Drew Erny <drewerny@gmail.com>
2014-09-27 11:41:32 -0500

    added Tasks and Dashboard controllers and views

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,7 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/view'
+
+  get 'tasks/new'
+
+  get 'tasks/edit'
+
   root to: "root#landing"
 
   devise_for :users
