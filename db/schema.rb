4092c80d05ca96add07d80b4b57617b0266417bf
Chris Keele <dev@chriskeele.com>
2014-09-27 18:05:56 -0500

    Add some relevant fields to tasks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927195352) do
+ActiveRecord::Schema.define(version: 20140927225342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20140927195352) do
     t.uuid     "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "estimated_hours", precision: 10, scale: 2, default: 0.0, null: false
+    t.decimal  "completed_hours", precision: 10, scale: 2, default: 0.0, null: false
+    t.datetime "due_at"
   end
 
   create_table "users", id: false, force: true do |t|
