cbb735fdcbe5aeef4a81c78280932a3bea969895
Drew Erny <drewerny@gmail.com>
2014-09-27 22:12:19 -0500

    implementing dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,18 @@
 class DashboardController < ApplicationController
   def view
+    @user = current_user
     today_sch = Scheduler.new(@user.tasks, {
       max_work_day: @user.max_work_day,
       min_work_period: @user.min_work_period,
       max_work_period: @user.max_work_period
     }
     @today = Scheduler.call
+
+    tom_sch = Scheduler.new(@user.tasks, {
+      max_work_day: @user.max_work_day,
+      min_work_period: @user.min_work_period,
+      max_work_period: @user.max_work_period
+    }
+    @tomorrow = Scheduler.call
   end
 end
